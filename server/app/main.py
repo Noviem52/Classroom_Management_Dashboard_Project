4f9405e262d011950d5b166ff205dfdfe4809a43
unknown <noviem5112004@gmail.com>
2026-09-15 13:52:48 +0800

    Update classes

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.api import departments, subjects
-from app.api import departments, subjects, classes
 from app.api import departments, subjects, classes, auth
 app = FastAPI(title="Classroom Management Dashboard API")
 
